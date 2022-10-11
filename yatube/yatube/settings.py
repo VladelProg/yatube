6fb6fd4cf69b0260da41aa911c4add5d46e605e2
Gilazov Fanis <agilazov@inbox.ru>
2022-10-11 21:56:12 +0300

    Revert "зазгрузка БД и статики"
    This reverts commit 39fc45390a0feea48c968108b5ab88857407b60a.

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -38,7 +37,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -126,7 +124,3 @@ CACHES = {
 }
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
