39fc45390a0feea48c968108b5ab88857407b60a
Gilazov Fanis <agilazov@inbox.ru>
2022-10-11 14:28:11 +0300

    зазгрузка БД и статики

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -124,3 +126,7 @@ CACHES = {
 }
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,6 +16,10 @@ if settings.DEBUG:
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
 
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
